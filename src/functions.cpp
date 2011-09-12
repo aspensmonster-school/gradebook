573ee6802dfe72953b541ea5c159b55e99142f14
Preston Maness <aggroskater@gmail.com>
2011-09-12 17:39:03 -0500

    Remove setValues and getValues

--- src/functions.cpp
+++ src/functions.cpp
@@ -6,5 +6,36 @@
  */
 
 
+#include<iostream>
+#include "functions.h"
 
+using namespace std;
+
+void getValues()
+{
+
+	cout << "\nName of Student: " << studentName << endl;
+	cout << "Student Social: " << studentSocial << endl;
+	cout << "Test 1: " << testOneGrade << endl;
+	cout << "Test 2: " << testTwoGrade << endl;
+	cout << "Final Exam: " << finalExamGrade << endl;
+	grade = testOneWeight*testOneGrade + testTwoWeight*testTwoGrade + finalExamWeight*finalExamGrade;
+	cout << "\nAccording to my calculations, you're a faggot-- I mean you earned a: " << grade;
+
+}
+
+void setValues()
+{
+	cout << "Welcome to the grade calculator!" << endl;
+	cout << "Provide student name (First-last): " ;
+		cin >> studentName;
+	cout << "Provide student social security number (XXX-XX-XXXX): " ;
+		cin >> studentSocial;
+	cout << "\nProvide student first test grade: ";
+		cin >> testOneGrade;
+	cout << "\nProvide student second test grade: ";
+		cin >> testTwoGrade;
+	cout << "\nProvide student final exam grade: ";
+		cin >> finalExamGrade;
+}
 
