f606d90bde0acb9bed44c24a49161cd535dcba5b
Preston Maness <aggroskater@gmail.com>
2011-09-14 23:52:07 -0500

    MOAR COMMENTS!

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
  *
  *  Created on: Aug 31, 2011
  *      Author: preston
+ *     Purpose: Obtains user input for some student info and related grades for the student, crunches a final grade,
+ *              then outputs the student info, component grades, and FINAL grade.
  */
 
 #include "functions.h" //contains vars and functions
